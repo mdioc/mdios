134a93af28a2a46ef0841effc1eac1185568f6bc
mdioc <oss@znta.io>
2025-11-11 17:17:16 -0500

    adding dnf plugins

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-dnf5 install -y tmux niri alacritty firefox NetworkManager NetworkManager-tui NetworkManager-wifi fuzzel wofi swayidle swaylock swaybg qt6ct brightnessctl pipewire wireplumber emacs-nox neovim entr flatpak distrobox
+dnf5 install -y tmux niri alacritty firefox NetworkManager NetworkManager-tui NetworkManager-wifi fuzzel wofi swayidle swaylock swaybg qt6ct brightnessctl pipewire wireplumber emacs-nox neovim entr flatpak distrobox dnf5-plugins
 
 # Use a COPR Example:
 #
